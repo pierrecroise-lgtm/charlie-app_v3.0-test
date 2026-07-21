98c5dec6e39cd59c209d154093b9987264c7edce
pierrecroise-lgtm <pierre.croise@gmail.com>
2026-07-21 23:08:26 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'charlie-v1';
-const ASSETS = ['./index.html', './manifest.json'];
+const ASSETS = ['./app.html', './manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
